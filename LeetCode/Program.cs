655d75fa3c1cf30a7c00517831f7450a578497f3
DharaniMuli <46985863+DharaniMuli@users.noreply.github.com>
2019-08-23 00:11:09 -0500

    Updated Valid Paranthesis

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -9,32 +9,38 @@ namespace LeetCode
         {
             // Start: Palindrome
 
-            //int x = Convert.ToInt32(Console.ReadLine());
-            //Palindrome p = new Palindrome();
-            //bool reversernumber = p.palindromeFinder(x);
+                //int x = Convert.ToInt32(Console.ReadLine());
+                //Palindrome p = new Palindrome();
+                //bool reversernumber = p.palindromeFinder(x);
 
             // End: Palindrome
 
             //Start: RomanToInteger
 
-            //String romannumber = Console.ReadLine();
-            //RomanToInteger rtoi = new RomanToInteger();
-            //rtoi.romanToInteger(romannumber);
+                 //String romannumber = Console.ReadLine();
+                 //RomanToInteger rtoi = new RomanToInteger();
+                 //rtoi.romanToInteger(romannumber);
 
             //End: RomanToInteger
 
 
             //Start: Longest Common Prefix
 
-            string[] myarray = { "Flower", "", "Flowering" };
+                string[] myarray = { "Flower", "", "Flowering" };
 
             //for(int i=0; i<= myarray.length;)
 
             //myarray = Convert.To Console.ReadLine();
-            LargestCommonPrefixCode lcp = new LargestCommonPrefixCode();
-            Console.WriteLine(lcp.findlargestCommonPrefix(myarray));
-            Console.ReadLine();
+            //LargestCommonPrefixCode lcp = new LargestCommonPrefixCode();
+            //Console.WriteLine(lcp.findlargestCommonPrefix(myarray));
+            //Console.ReadLine();
             //End: Longest Common Prefix
+
+            //Start: Valid Paranthesis
+            string s = "{}(}";
+            ValidParathesis vp = new ValidParathesis();
+            vp.IsValid(s);
+            //End: Valid Paranthesis
         }
     }
 }
